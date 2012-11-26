c60023615931a5b6daf05164a9de10185642724e
Javier Seixas <javierseixas@pinguseixas.(none)>
2012-11-27 00:05:01 +0100

    Setup Doctrine ORM Provider

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -7,9 +7,18 @@ use JavierSeixas\PracBundle\Controller\DescripcionsControllerProvider;
 use JavierSeixas\PracBundle\Controller\CategoriesControllerProvider;
 use JavierSeixas\PracBundle\Controller\ClassificacioControllerProvider;
 use JavierSeixas\PracBundle\Controller\EstocsControllerProvider;
+use Symfony\Component\Yaml\Parser;
 
+// Inicialitza aplicació
 $app = new Silex\Application();
 
+// Inicialitza pasejador d'arxius yaml
+$yaml = new Parser();
+
+// Agafa contingut arxius de configuració yaml
+$config = $yaml->parse(file_get_contents(__DIR__.'/config/config.yml'));
+
+// Munta els controladors
 $app->mount('/productes', new ProductesControllerProvider());
 $app->mount('/descripcions', new DescripcionsControllerProvider());
 $app->mount('/categories', new CategoriesControllerProvider());
@@ -17,6 +26,8 @@ $app->mount('/classificacio', new ClassificacioControllerProvider());
 $app->mount('/estocs', new EstocsControllerProvider());
 
 $app['debug'] = true;
+
+// Carrega els diferents Proveidors de servei
 
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__.'/Resources/views',
@@ -26,11 +37,19 @@ $app->before(function () use ($app) {
     $app['twig']->addGlobal('layout', $app['twig']->loadTemplate('layout.html.twig'));
 });
 
-$app->register(new Silex\Provider\DoctrineServiceProvider(), array(
-    'db.options' => array(
-        'driver'   => 'pdo_mysql',
-        'path'     => __DIR__.'/app.db',
-    ),
+$app->register(new \Silex\Provider\DoctrineServiceProvider(), array(
+    'db.options'  => $config['database']
+));
+
+$app->register(new \Nutwerk\Provider\DoctrineORMServiceProvider(), array(
+    'db.orm.proxies_dir'           => __DIR__.'/cache/doctrine/Proxy',
+    'db.orm.proxies_namespace'     => 'DoctrineProxy',
+    'db.orm.auto_generate_proxies' => true,
+    'db.orm.entities'              => array(array(
+        'type'      => 'yml',
+        'path'      => __DIR__.'/../src/JavierSeixas/PracBundle/Resources/mapping',
+        'namespace' => 'JavierSeixas\PracBundle\Entity',
+    )),
 ));
 
 
